9726acb1b72a6b670a2ec558d59a90c209bb637d
Deonna Hodges <deonna@learnersguild.org>
2016-10-09 23:47:27 -0700

    refactor(note): Individual notes decomposed.

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -2,10 +2,14 @@
 
 import React from 'react'
 
+import Note from './Note'
+
 export default ({notes}) => (
   <ul>
     {notes.map(note =>
-      <li key={note.id}>{note.task}</li>
+      <li key={note.id}>
+        <Note task={note.task} />
+      </li>
     )}
   </ul>
 )
