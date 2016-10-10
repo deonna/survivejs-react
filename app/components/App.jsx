d303550442bc4a4cbceb7085fa69652fd6fe7259
Deonna Hodges <deonna@learnersguild.org>
2016-10-09 22:50:39 -0700

    refactor(app): Moving Notes logic into App.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,23 @@
 // @flow
 import React from 'react'
+import uuid from 'uuid'
+
 import Notes from './Notes'
+
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn React'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Do laundry'
+  }
+]
 
 export default () => (
   <div>
     <button onClick={() => console.log('add note')}>+</button>
-    <Notes />
+    <Notes notes={notes} />
   </div>
 )
