e31fbd46e07a93b2c8dfd07da67043e129300e6c
Deonna Hodges <deonna@learnersguild.org>
2016-10-09 23:04:06 -0700

    refactor(app): Converted to class-based component.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,20 +4,32 @@ import uuid from 'uuid'
 
 import Notes from './Notes'
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
-  }
-]
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-export default () => (
-  <div>
-    <button onClick={() => console.log('add note')}>+</button>
-    <Notes notes={notes} />
-  </div>
-)
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do Laundry'
+        }
+      ]
+    }
+  }
+
+  render() {
+    const {notes} = this.state
+
+    return (
+      <div>
+        <button onClick={() => console.log('add note')}>+</button>
+        <Notes notes={notes} />
+      </div>
+    )
+  }
+}
