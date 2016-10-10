7258298a5ac26570d357b507e583db7ffad79fd1
Deonna Hodges <deonna@learnersguild.org>
2016-10-09 22:03:31 -0700

    feat(notes): Adding fake notes.

---
+++ app/components/Notes.jsx
@@ -0,0 +1,23 @@
+// @flow
+
+import React from 'react'
+import uuid from 'uuid'
+
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn React'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Do laundry'
+  }
+]
+
+export default () => (
+  <ul>
+    {notes.map(note =>
+      <li key={note.id}>{note.task}</li>
+    )}
+  </ul>
+)
