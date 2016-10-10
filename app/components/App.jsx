da0671472f8053cd65d9d268f73a196245aa8d77
Deonna Hodges <deonna@learnersguild.org>
2016-10-09 22:10:27 -0700

    feat(add-note): Add note button prints to console upon click.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,4 +2,9 @@
 import React from 'react'
 import Notes from './Notes'
 
-export default () => <Notes />
+export default () => (
+  <div>
+    <button onClick={() => console.log('add note')}>+</button>
+    <Notes />
+  </div>
+)
