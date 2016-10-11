bfcc2f2790eefd86eb13045ac5157ace4c5a4ec4
Deonna Hodges <deonna@learnersguild.org>
2016-10-10 18:17:26 -0700

    feat(editing): Editable component stubbed out.

---
+++ app/components/Editable.jsx
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react'
+
+export default ({editing, value, onEdit, ...props}) => {
+  if(editing) {
+    return <Edit value={value} onEdit={onEdit} {...props} />
+  }
+
+  return <span {...props}value: {value}</span></span>
+}
+
+const Edit = ({onEdit = () => {}, value, ...props}) => (
+  <div onClick={onEdit} {...props}>
+    <span>edit: {value}</span>
+  </div>
+)
